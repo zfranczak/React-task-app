a3bdb6c6ef72d61d3f3c5e36653c8db330e6120c
Zak Franczak <zakfranczak@gmail.com>
2023-03-15 12:29:18 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-function App() {
-  const name = 'Brad';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
 
   return (
-    <div className="container">
-      <h1>Hello from React</h1>
-      <h2>Hello {name}</h2>
+    <div className='container'>
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
