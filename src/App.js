b1e0749bc893614876897e870363963e4f8ec3c1
Zak Franczak <zakfranczak@gmail.com>
2023-03-17 13:54:49 -0500

    Added task creator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,19 +26,31 @@ const App = () => {
     },
   ])
 
-  //Delete Task
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  //Toggle Reminder
+  // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id)
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
   }
 
   return (
     <div className='container'>
       <Header />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
