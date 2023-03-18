f1c1ae95ed176fa96b759eccde2671798d942c79
Zak Franczak <zakfranczak@gmail.com>
2023-03-18 17:19:45 -0500

    Added task adder function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click');
-  };
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='Green' text='Add' onClick={onClick} />
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
-  );
-};
+  )
+}
 
 Header.defaultProps = {
   title: 'Task Tracker',
-};
+}
 
 Header.propTypes = {
   title: PropTypes.string,
-};
+}
 
-export default Header;
+export default Header
